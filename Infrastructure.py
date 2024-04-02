927b7f1441a6f43666edba5cc04dd4ac16b4905a
Jaeden Carpenter <112436035+carpenterjaeden@users.noreply.github.com>
2024-04-02 09:05:03 -0700

    Update Infrastructure.py

--- Infrastructure.py
+++ Infrastructure.py
@@ -1,1 +1,26 @@
 # Contains classes for all the different types of roadways
+class Infrastructure:
+    def __init__(self, type, size):
+        self.type = type
+        self.size = size
+        self.roads = []
+
+    def add_road(self, road):
+        self.roads.append(road)
+
+    def initialize_infrastructure(self):
+        if self.type == "grid":
+            # Create a grid-like structure of roads
+            for i in range(self.size):
+                for j in range(self.size):
+                    self.add_road(Road(f"Road {i}-{j}"))
+                    if i < self.size - 1:
+                        self.add_road(Road(f"Avenue {i}-{j}"))
+        elif self.type == "highway":
+            
+
+class Road:
+    def __init__(self, name):
+        self.name = name
+
+
