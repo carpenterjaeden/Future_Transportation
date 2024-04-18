7d495dcd5c4b2b14beaae68e5d524e8c78c20d03
Jaeden Carpenter <112436035+carpenterjaeden@users.noreply.github.com>
2024-04-18 16:12:40 -0700

    comments and small methods

--- User.py
+++ User.py
@@ -1,15 +1,23 @@
 # Class for the User
-# time is time it takes to get to the destination from out of the door
-# size is the amount of people
+# Optimized to have the easiest, fewest questions while still being able to get a good idea for the best mode of transportation
 class User:
-    def __init__(self, age, distance, time, size):
-        self.age = age
-        self.size = size
-        self.distance = distance
-        self.time = time
+    def __init__(self, age, distance, time, size, disability):
+        self.age = age # age of user will affect how appropriate some modes of transportation are (ie: under 16 cannot drive)
+        self.size = size # number of people travelling
+        self.distance = distance # approximate distance in miles
+        self.time = time # scale 1-10 of how fast the user wants to go
+        self.disability = disability # boolean that determines whether the user cannot operate a motor vehicle
 
     # method that takes user input and uses it to instatiate the class
-    def input_parameters(self):
+    @classmethod
+    def input_parameters(cls):
+        age = int(input("Enter your age: "))
+        distance = float(input("Enter the approximate distance to your destination in miles: "))
+        time = float(input("How important is the time of travel (1-10): "))
+        size = int(input("Enter the number of people travelling: "))
+        disability = bool(input("Are there any impairments or disabilities that affect your ability to operate a motor vehicle? (True/False): "))
+
+        return cls(age, distance, time, size)
     
 
 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import Infrastructure
 import Transportation
+import User
 
 # instantiate different modes of transportation (may be moved to a different file)
 # instantiate different infrastructures (may be moved to a different file)
@@ -7,3 +8,6 @@ import Transportation
 # name, cost, size, time_to_start, parking, transport_time, env_impact, traffic, safety, maintain, dis_sup
 train = Transportation('train', 3, 50, 7, 1, 7, 2, 1, 8, 1, True);
 car =   Transportation('car'  , 6, 4 , 3, 7, 5, 5, 6, 7, 6, True);
+
+# this is how to instantiate a user using input parameters
+user = User.input_parameters()
