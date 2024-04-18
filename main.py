7a501abf96a5019c6fe69f76c6c443d329539161
SushiDeliveryGuy <112435954+SushiDeliveryGuy@users.noreply.github.com>
2024-04-18 15:21:18 -0700

    train and car classes

---
+++ main.py
@@ -0,0 +1,3 @@
+# name, cost, size, time_to_start, parking, transport_time, env_impact, traffic, safety, maintain, dis_sup
+train = Transportation('train', 3, 50, 7, 1, 7, 2, 1, 8, 1, True);
+car =   Transportation('car'  , 6, 4 , 3, 7, 5, 5, 6, 7, 6, True);
