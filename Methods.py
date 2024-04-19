26c569038c2e08f300416a8b1b45e0db84cc78bf
SushiDeliveryGuy <s4k3@arizona.edu>
2024-04-18 18:48:03 -0700

    6 starting modes

--- Methods.py
+++ Methods.py
@@ -3,12 +3,30 @@ import Transportation
 import User
 
 def set_modes_of_transportation():
-    # name, cost, size, time_to_start, parking, transport_time, env_impact, traffic, safety, maintain, dis_sup
+    # 1. name
+    # 2. cost 
+    # 3. size
+    # 4. time_to_start
+    # 5. parking
+    # 6. transport_time
+    # 7. env_impact
+    # 8. traffic
+    # 9. safety
+    # 10. maintain
+    # 11. dis_sup
 
     mot = [] # array that contains the modes of transportation that will be used for loops
-    train = Transportation('train', 3, 50, 7, 1, 7, 2, 1, 8, 1, True)
+    train = Transportation('train', 3, 70, 7, 0, 7, 2, 1, 6, 0, True)
     mot.append(train)
-    car =   Transportation('car'  , 6, 4 , 3, 7, 5, 5, 6, 7, 6, True)
+    car =   Transportation('car'  , 6, 4 , 1, 7, 5, 5, 6, 7, 6, True) # personal car
     mot.append(car)
+    taxi =  Transportation('taxi' , 5, 2 , 4, 0, 5, 5, 6, 5, 0, True) # taxi, uber, etc.
+    mot.append(taxi)
+    plane = Transportation('plane', 8, 200, 8, 2, 9, 6, 1, 8, 0, True)
+    mot.append(plane)
+    walk =  Transportation('walk', 0, 1, 0, 0, 9, 0, 1, 5, 10, False) # I assume false for dis_sup?
+    mot.append(walk)
+    bike =  Transportation('bike', 1, 1, 0, 2, 8, 0, 3, 4, 9, False)
+    mot.append(bike)
 
     return mot
