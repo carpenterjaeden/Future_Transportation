f527034c203b39411e8bd23f3ac180f43c6b28bf
Timothy Haines <timothyhaines@arizona.edu>
2024-05-01 00:58:59 -0700

    error checking done

--- User.py
+++ User.py
@@ -16,17 +16,89 @@ class User:
     # method that takes user input and uses it to instatiate the class
     @classmethod
     def input_parameters(cls):
-        age = int(input("Enter your age: "))
-        distance = float(input("Enter the approximate distance to your destination in miles: "))
-        time = float(input("How important is the time of travel (0-10): "))
-        size = int(input("Enter the number of people travelling: "))
-        disability_input = input("Are there any impairments or disabilities that affect your ability to operate a motor vehicle? (True/False): ")
-        disability = (disability_input.lower() == 'true')
-        traffic = int(input("What is the estimated amount of traffic? (0-10): "))
-        cost = int(input("How important is the cost? (0-10): "))
-        infrastructure_name = input("What kind of infrastructure are you travelling in? highway or grid: ")
+        age = distance = time = size = disability_input = traffic = cost = infrastructure_name = None
+        while age is None:
+            try:
+                age = int(input("Enter your age: "))
+                if age < 0:
+                    raise ValueError("Your age should be greater than -1.")
+            except ValueError:
+                print("Please enter a valid age.")
+                age = None;
+        # age = int(input("Enter your age: "))
+        # distance = float(input("Enter the approximate distance to your destination in miles: "))
+        # time = float(input("How important is the time of travel (0-10): "))
+        # size = int(input("Enter the number of people travelling: "))
+        # disability_input = input("Are there any impairments or disabilities that affect your ability to operate a motor vehicle? (True/False): ")
+        # disability = (disability_input.lower() == 'true')
+        # traffic = int(input("What is the estimated amount of traffic? (0-10): "))
+        # cost = int(input("How important is the cost? (0-10): "))
+        # infrastructure_name = input("What kind of infrastructure are you travelling in? highway or grid: ")
+        
+        while distance is None:
+            try:
+                distance = float(input("Enter the approximate distance to your destination in miles: "))
+                if distance < 0:
+                    raise ValueError("Time distance should be greater than 0.")
+            except ValueError:
+                print("Please enter a valid distance.")
+                distance = None
+    
+        while time is None:
+            try:
+                time = float(input("How important is the time of travel from lowest to highest priority (0-10): "))
+                if time < 0 or time > 10:
+                    raise ValueError("Time importance should be between 0 and 10.")
+            except ValueError as e:
+                print(e)
+                time = None
+    
+        while size is None:
+            try:
+                size = int(input("Enter the number of people travelling: "))
+                if size < 0:
+                    raise ValueError("There should be more than 0 people traveling.")
+            except ValueError:
+                print("Please enter a valid number of people.")
+                size = None
 
-        return cls(age, distance, time, size, disability, traffic, cost, infrastructure_name)
+        while disability_input is None:
+            try:
+                disability_input = input("Are there any impairments or disabilities that affect your ability to operate a motor vehicle? (True/False): ")
+                if disability_input.lower() not in ['true', 'false']:
+                    raise ValueError("Please enter 'True' or 'False'.")
+            except ValueError:
+                print("Please enter True or False.")
+                disability_input = None
+
+        while traffic is None:
+            try:
+                traffic = int(input("What is the estimated amount of traffic from lowest to highest? (0-10): "))
+                if traffic < 0 or traffic > 10:
+                    raise ValueError("Traffic estimate should be between 0 and 10.")
+            except ValueError as e:
+                print(e)
+                traffic = None
+
+        while cost is None:
+            try:
+                cost = int(input("How important is the cost from lowest priority to highest? (0-10): "))
+                if cost < 0 or cost > 10:
+                    raise ValueError("Cost importance should be between 0 and 10.")
+            except ValueError as e:
+                print(e)
+                cost = None
+
+        while infrastructure_name is None:
+            try:
+                infrastructure_name = input("What kind of infrastructure are you travelling in? highway or grid: ")
+                if infrastructure_name.lower() not in ['highway', 'grid']:
+                    raise ValueError("Please enter 'highway' or 'grid'.")
+            except ValueError as e:
+                print(e)
+                infrastructure_name = None
+
+        return cls(age, distance, time, size, disability_input, traffic, cost, infrastructure_name)
     
     @classmethod
     def random_parameters(cls):
